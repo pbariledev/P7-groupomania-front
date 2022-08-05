be1e116b48858dad9f41675c77a0c95a99fb5a96
Pierre BARILE <pbariledev@gmail.com>
2022-08-05 14:53:19 +0200

    W login

--- src/components/Log/LoginForm.jsx
+++ src/components/Log/LoginForm.jsx
@@ -3,26 +3,23 @@ import axios from 'axios'
 import { useState } from 'react';
 
 const LoginForm = () => {
+
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     
     const handleLogin = (e) =>{
-        e.prevent.default();
+        e.preventDefault();
         const emailError= document.querySelector ('.email.error');
         const passwordError= document.querySelector ('.password.error');
 
-        axios({
-            method : "post",
-            url: `${process.env.REACT_APP_API_URL}api/user/login`,
-            withCredentials: true,
-            data: {
-                email,
-                password, 
-            },      
+        axios.post("http://localhost:5000/api/auth/login",{
+                email: email,
+                password: password, 
         })
             .then ((res)=>{
                 console.log("reponse");
                 if (res.data.errors) {
+                    console.log("y a error")
                     emailError.innerHTML= res.data.errors.email;
                     passwordError.innerHTML= res.data.errors.password;
                 } else {
@@ -42,7 +39,8 @@ const LoginForm = () => {
                 type="text" 
                 name="email" 
                 id="email" 
-                onChange={(e)=> setEmail (e.target.value)} 
+                onChange={(e)=> setEmail (e.target.value)}
+                value ={email}
             />
             <div className="email error"></div>
             <br />
@@ -52,7 +50,8 @@ const LoginForm = () => {
                 type="password" 
                 name="password" 
                 id="password" 
-                onChange={(e)=> setPassword (e.target.value)}  
+                onChange={(e)=> setPassword (e.target.value)}
+                value={password}
             />
             <div className="password error"></div>
             <br />
