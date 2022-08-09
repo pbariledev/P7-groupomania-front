2f638a0c0baaea0281a24211def93c9ae782353b
Pierre BARILE <pbariledev@gmail.com>
2022-08-09 10:52:57 +0200

    start myprofil2

--- src/pages/Myprofil/index.jsx
+++ src/pages/Myprofil/index.jsx
@@ -8,26 +8,23 @@ import axios from 'axios'
 export const Profil = () => {
     const auth = useAuth()
 
-    const [data, setData]= useState([]);
     const userId= localStorage.getItem('userId')
+    console.log(userId)
+
 
     const fetchData = () => {
         axios
             .get(`http://localhost:5000/api/auth/myprofil?=${userId}`)
             .then((res)=> {
                 console.log(res)
-                setData (res.data)
             })
             .catch((err)=>{
                 console.log(err)
-                console.log(userId)
-
             })
     }
     useEffect(()=>{
         fetchData()
     },[])
-
     
     return (
         <div className='profil_container'>
